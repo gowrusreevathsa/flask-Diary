505bc88c160e0d11b0de565033471be11ca90544
Sreevathsa <sreevathsagowru@gmail.com>
2020-06-03 14:15:47 +0530

    Working version

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ import datetime as dt
 app = Flask(__name__)
 title = 'Diary'
 heading = 'Your Diary'
+day = None
+month = None
+year = None
+monthName = None
+id = None
 
 
 @app.route('/write')
@@ -16,7 +21,13 @@ def write():
 
 @app.route('/showNotes/<dateid>')
 def showNotes(dateid):
-    
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    db = client['dbDiary']
+    coll = db['diaryNotes']
+    showId = str(dateid)
+    i = coll.find_one({"_id":showId})
+    dayT, monthT, yearT = detailsFromDateId(dateid)
+    return render_template('index.html', day = dayT, month = dt.date(2020, int(monthT), 1).strftime('%B'), year = yearT, preText = i['notes'])
 
 @app.route('/addNotes/', methods = ['POST'])
 def addNotes():
@@ -24,14 +35,9 @@ def addNotes():
     if request.method == 'POST':
         notes = request.form
         notes = notes['notes']
-    today = dt.datetime.now()
-    year = today.year
-    month = today.strftime("%B")
-    day = today.day
     client = MongoClient("mongodb://127.0.0.1:27017")
     db = client['dbDiary']
     coll = db['diaryNotes']
-    id = str(today.year) + str(today.month) + str(today.day)
     data = [
         {
             "_id":id,
@@ -43,23 +49,44 @@ def addNotes():
     else:
         res = coll.insert(data)
     print(res)
+
+    for i in coll.find():
+        print(i)
     
-    return '<h5> Success </h5>'
+    return redirect(url_for('showNotes', dateid = id))
 
 @app.route('/')
 def index():
     client = MongoClient("mongodb://127.0.0.1:27017")
     db = client['dbDiary']
     print("Created or Connected to the database: dbDiary")
-    today = dt.datetime.now()
     coll = db['diaryNotes']
-    id = str(today.year) + str(today.month) + str(today.day)
     preText = ''
     if coll.count_documents({"_id":id}, limit = 1):
         i = coll.find_one({'_id':id})
         print(i['notes'])
         preText = i["notes"]
-    return render_template('index.html', day = today.day ,month = today.strftime("%B"), year = today.year, preText = preText)
+    return render_template('index.html', day = day ,month = monthName, year = year, preText = preText)
+
+def getID():
+    today = dt.datetime.now()
+    day = today.day
+    month = today.month
+    if day < 10:
+        day = '0' + str(day)
+    if month < 10:
+        month = '0' + str(month)
+    year = str(today.year)
+    return day, month, year, day + month + year
+
+def detailsFromDateId(dateid):
+    day = dateid[:2]
+    month = dateid[2:4]
+    year = dateid[4:]
+
+    return day, month, year
 
 if __name__ == "__main__":
+    day, month, year, id = getID()
+    monthName = dt.datetime.now().strftime("%B")
     app.run(debug = True)
